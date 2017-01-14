2ebb68ddcf7100f949e808bea8eff99dff98c307
Gagandeep Singh <gagan.singh04@gmail.com>
2017-01-13 16:08:32 -0800

    Added "Change feature layer renderer" sample

---
+++ arcgis-runtime-samples-macos/Features/Change feature layer renderer/ChangeFeatureLayerRendererVC.swift
@@ -0,0 +1,65 @@
+//
+// Copyright 2016 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Cocoa
+import ArcGIS
+
+class ChangeFeatureLayerRendererVC: NSViewController {
+
+    @IBOutlet var mapView:AGSMapView!
+    
+    private var map:AGSMap!
+    private var featureLayer:AGSFeatureLayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //initialize map with topographic basemap
+        self.map = AGSMap(basemap: AGSBasemap.topographicBasemap())
+        
+        //initial viewpoint
+        self.map.initialViewpoint = AGSViewpoint(targetExtent: AGSEnvelope(XMin: -1.30758164047166E7, yMin: 4014771.46954516, xMax: -1.30730056797177E7
+            , yMax: 4016869.78617381, spatialReference: AGSSpatialReference.webMercator()))
+        
+        //assign map to the map view's map
+        self.mapView.map = self.map
+        
+        //initialize feature table using a url to feature server url
+        let featureTable = AGSServiceFeatureTable(URL: NSURL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/PoolPermits/FeatureServer/0")!)
+        
+        //initialize feature layer with feature table
+        self.featureLayer = AGSFeatureLayer(featureTable: featureTable)
+        
+        //add the feature layer to the operational layers on the map view
+        self.map.operationalLayers.addObject(self.featureLayer)
+    }
+    
+    //MARK: - Actions
+    
+    @IBAction private func applyRenderer(_ sender:NSButton) {
+        //create a symbol to be used in the renderer
+        let symbol = AGSSimpleLineSymbol(style: AGSSimpleLineSymbolStyle.Solid, color: NSColor.blueColor(), width: 2)
+        //create a new renderer using the symbol just created
+        let renderer = AGSSimpleRenderer(symbol: symbol)
+        //assign the new renderer to the feature layer
+        self.featureLayer.renderer = renderer
+    }
+    
+    @IBAction private func resetRenderer(_ sender:NSButton) {
+        //reset the renderer to default
+        self.featureLayer.resetRenderer()
+    }
+}
---
+++ arcgis-runtime-samples-macos/Features/Change feature layer renderer/README.md
@@ -0,0 +1,17 @@
+#Change feature layer renderer
+
+This sample demonstrates how to override and reset a renderer of a feature layer.
+
+##How to use the sample
+
+Use the buttons in the bottom to apply or reset the renderer.
+
+![](image1.png)
+
+##How it works
+
+`AGSFeatureLayer` has a property called `renderer` of type `AGSRenderer` you can set to override the renderer. And it provides a method called `resetRenderer` to reset to the original one.
+
+
+
+
