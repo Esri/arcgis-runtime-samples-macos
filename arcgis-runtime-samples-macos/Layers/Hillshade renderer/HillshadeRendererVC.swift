492955b7c4a14c0261f0ffd1e26615c676e91b66
Gagandeep Singh <gagan.singh04@gmail.com>
2017-01-18 17:13:22 -0800

    Added "Hillshade Renderer" sample

---
+++ arcgis-runtime-samples-macos/Layers/Hillshade renderer/HillshadeRendererVC.swift
@@ -0,0 +1,87 @@
+//
+// Copyright 2016 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Cocoa
+import ArcGIS
+
+class HillshadeRendererVC: NSViewController {
+
+    @IBOutlet var mapView:AGSMapView!
+    @IBOutlet var altitudeSlider:NSSlider!
+    @IBOutlet var azimuthSlider:NSSlider!
+    @IBOutlet var altitudeLabel:NSTextField!
+    @IBOutlet var azimuthLabel:NSTextField!
+    @IBOutlet var slopeType:NSPopUpButton!
+    
+    private var map:AGSMap!
+    private var rasterLayer:AGSRasterLayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        //raster layer
+        let raster = AGSRaster(name: "srtm", extension: "tiff")
+        self.rasterLayer = AGSRasterLayer(raster: raster)
+        
+        //initial map with raster layer as basemap
+        self.map = AGSMap(basemap: AGSBasemap(baseLayer: self.rasterLayer))
+        
+        //assign map to map view
+        self.mapView.map = self.map
+        
+        //initial renderer
+        let renderer = AGSHillshadeRenderer(altitude: 45, azimuth: 315, zFactor: 0.000016, slopeType: .None, pixelSizeFactor: 1, pixelSizePower: 1, outputBitDepth: 8)
+        rasterLayer.renderer = renderer
+    }
+    
+    func selectedSlope() -> AGSSlopeType {
+        switch self.slopeType.indexOfSelectedItem {
+        case 0:
+            return .None
+        case 1:
+            return .Degree
+        case 2:
+            return .PercentRise
+        default:
+            return .Scaled
+        }
+    }
+    
+    func applyRenderer(altitude: Double, azimuth: Double, slopeType: AGSSlopeType) {
+        //initialize hill shade renderer with provided settings
+        let renderer = AGSHillshadeRenderer(altitude: altitude, azimuth: azimuth, zFactor: 0.000016, slopeType: slopeType, pixelSizeFactor: 1, pixelSizePower: 1, outputBitDepth: 8)
+        
+        //assign renderer to raster layer
+        self.rasterLayer.renderer = renderer
+    }
+    
+    //MARK: - Actions
+    
+    @IBAction func applyAction(_ sender:NSButton) {
+        let altitude = self.altitudeSlider.doubleValue
+        let azimuth = self.azimuthSlider.doubleValue
+        
+        self.applyRenderer(altitude, azimuth: azimuth, slopeType: self.selectedSlope())
+    }
+    
+    @IBAction func altitudeSliderAction(_ sender:NSSlider) {
+        self.altitudeLabel.stringValue = "\(sender.integerValue)"
+    }
+    
+    @IBAction func azimuthSliderAction(_ sender:NSSlider) {
+        self.azimuthLabel.stringValue = "\(sender.integerValue)"
+    }
+}
---
+++ arcgis-runtime-samples-macos/Layers/Hillshade renderer/README.md
@@ -0,0 +1,19 @@
+#Hillshade renderer
+
+This sample demonstrates how to use hillshade renderer on a raster
+
+##How to use the sample
+
+The sample allows you to change the `Altitude`, `Azimuth` and `Slope type`. You can click on the `Apply` button to update the raster.
+
+![](image1.png)
+![](image2.png)
+
+##How it works
+
+The sample uses `AGSHillshadeRenderer` class to generate hillshade renderers. The settings provided by the user are put in the initializer `initWithAltitude:azimuth:zFactor:slopeType:pixelSizeFactor:pixelSizePower:outputBitDepth:` to get a new renderer and the renderer is then set on the raster. 
+
+
+
+
+
