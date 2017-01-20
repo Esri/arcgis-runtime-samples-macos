9d74e841f2f5375270defa1387832b4801231bd2
Gagandeep Singh <gagan.singh04@gmail.com>
2017-01-19 16:52:58 -0800

    Added two samples: - Feature collection layer - Feature collection layer (query)

---
+++ arcgis-runtime-samples-macos/Features/Feature collection layer (query)/FeatureCollectionLayerQueryVC.swift
@@ -0,0 +1,71 @@
+//
+// Copyright 2016 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Cocoa
+import ArcGIS
+
+class FeatureCollectionLayerQueryVC: NSViewController {
+
+    @IBOutlet var mapView:AGSMapView!
+    
+    private var featureTable:AGSServiceFeatureTable!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //initialize map with basemap
+        let map = AGSMap(basemap: AGSBasemap.oceansBasemap())
+        
+        //assign map to the map view
+        self.mapView.map = map
+        
+        //initialize service feature table to be queried
+        self.featureTable = AGSServiceFeatureTable(URL: NSURL(string: "http://sampleserver6.arcgisonline.com/arcgis/rest/services/Wildfire/FeatureServer/0")!)
+        
+        //create query parameters
+        let queryParams = AGSQueryParameters()
+        
+        // 1=1 will give all the features from the table
+        queryParams.whereClause = "1=1"
+        
+        //show progress indicator
+        self.view.window?.showProgressIndicator()
+        
+        //query feature from the table
+        self.featureTable.queryFeaturesWithParameters(queryParams) { [weak self] (queryResult: AGSFeatureQueryResult?, error: NSError?) in
+        
+            //hide progress indicator
+            self?.view.window?.hideProgressIndicator()
+            
+            if let error = error {
+                print(error)
+            }
+            else {
+                //create a feature collection table fromt the query results
+                let featureCollectionTable = AGSFeatureCollectionTable(featureSet: queryResult!)
+                
+                //create a feature collection from the above feature collection table
+                let featureCollection = AGSFeatureCollection(featureCollectionTables: [featureCollectionTable])
+                
+                //create a feature collection layer
+                let featureCollectionLayer = AGSFeatureCollectionLayer(featureCollection: featureCollection)
+                
+                //add the layer to the operational layers array
+                self?.mapView.map?.operationalLayers.addObject(featureCollectionLayer)
+            }
+        }
+    }
+}
