8d26313378640d16fd5fbd80ff23c04615fe94c5
Philip Gruenler <pgruenler@esri.com>
2017-06-27 11:45:56 -0700

    add terrain exaggeration sample

---
+++ arcgis-runtime-samples-macos/Scenes/Terrain exaggeration/TerrainExaggerationViewController.swift
@@ -0,0 +1,45 @@
+//
+// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Cocoa
+import ArcGIS
+
+class TerrainExaggerationViewController: NSViewController {
+
+    @IBOutlet weak var exaggerationValue: NSTextField!
+    @IBOutlet weak var exaggerationSlider: NSSlider!
+    @IBOutlet weak var sceneView: AGSSceneView!
+    let scene = AGSScene(basemapType: .streets)
+    let surface = AGSSurface()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let elevation = AGSArcGISTiledElevationSource(url: URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!)
+        surface.elevationSources.append(elevation)
+        scene.baseSurface = surface
+        
+        self.sceneView.scene = scene
+        
+        let initialLocation = AGSPoint(x: -119.94891542688772, y: 46.75792111605992, spatialReference: sceneView.spatialReference)
+        let camera = AGSCamera(lookAt: initialLocation, distance: 15000.0, heading: 40.0, pitch: 60.0, roll: 0.0)
+        sceneView.setViewpointCamera(camera)
+    }
+    
+    @IBAction func sliderAction(_ sender: NSSlider) {
+        surface.elevationExaggeration = sender.floatValue
+        exaggerationValue.stringValue = String(format: "%.1fx", sender.floatValue)
+    }
+}
