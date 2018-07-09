6155471d4d802e1c717b45bdc7f39def1039ee47
Nicholas Furness <nfurness@nixta.com>
2018-07-09 10:50:42 -0400

    Add Feature layer (shapefile) sample (#121)
    Also rename the old Feature Layer (Shapefile) sample as Symbolize Shapefile.

--- arcgis-runtime-samples-macos/Layers/Symbolize shapefile/SymbolizeShapefileViewController.swift
+++ arcgis-runtime-samples-macos/Layers/Symbolize shapefile/SymbolizeShapefileViewController.swift
@@ -1,4 +1,4 @@
-// Copyright 2018 Esri.
+// Copyright 2017 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,39 +14,37 @@
 
 import ArcGIS
 
-class FeatureLayerShapefileViewController: NSViewController {
-
+class SymbolizeShapefileViewController: NSViewController {
+    
     @IBOutlet weak var mapView: AGSMapView!
-
-    var map:AGSMap
-    var featureLayer:AGSFeatureLayer
-
-    required init?(coder aDecoder: NSCoder) {
-        // Instantiate a map using a basemap.
-        map = AGSMap(basemap: AGSBasemap.streetsVector())
-
-        // Create a shapefile feature table from a named bundle resource.
-        let shapefileTable = AGSShapefileFeatureTable(name: "Public_Art")
-
-        // Create a feature layer for the shapefile feature table.
-        featureLayer = AGSFeatureLayer(featureTable: shapefileTable)
-
-        // Add the layer to the map.
-        map.operationalLayers.add(featureLayer)
-
-        super.init(coder: aDecoder)
-    }
-
+    
+    var featureLayer:AGSFeatureLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Instantiate a map using a basemap.
+        let map = AGSMap(basemap: AGSBasemap.streetsVector())
+        
+        // Create a shapefile feature table from a named bundle resource.
+        let shapefileTable = AGSShapefileFeatureTable(name: "Subdivisions")
+        
+        // Create a feature layer for the shapefile feature table.
+        let shapefileLayer = AGSFeatureLayer(featureTable: shapefileTable)
+        
+        // Add the layer to the map.
+        map.operationalLayers.add(shapefileLayer)
+        
         // Display the map in the map view.
         mapView.map = map
-
+        
         // Zoom the map to the Shapefile's extent.
-        zoom(mapView: mapView, to: featureLayer)
+        zoom(mapView: mapView, to: shapefileLayer)
+        
+        // Hold on to the layer to set its symbology later.
+        featureLayer = shapefileLayer
     }
-
+    
     func zoom(mapView:AGSMapView, to featureLayer:AGSFeatureLayer) {
         // Ensure the feature layer's metadata is loaded.
         featureLayer.load { error in
@@ -54,11 +52,23 @@ class FeatureLayerShapefileViewController: NSViewController {
                 print("Couldn't load the shapefile \(error!.localizedDescription)")
                 return
             }
-
+            
             // Once the layer's metadata has loaded, we can read its full extent.
             if let initialExtent = featureLayer.fullExtent {
                 mapView.setViewpointGeometry(initialExtent)
             }
         }
     }
+    
+    @IBAction func setShapefileSymbol(_ sender: Any) {
+        if let layer = featureLayer {
+            // Create a new yellow fill symbol with a red outline.
+            let outlineSymbol = AGSSimpleLineSymbol(style: .solid, color: .red, width: 1)
+            let fillSymbol = AGSSimpleFillSymbol(style: .solid, color: .yellow, outline: outlineSymbol)
+            
+            // Create a new renderer using this symbol and set it on the layer.
+            layer.renderer = AGSSimpleRenderer(symbol: fillSymbol)
+        }
+    }
+    
 }
