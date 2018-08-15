f4c5536f4fb3d91f19df9a6a4d8962e78b3d4a21
Philip Ridgeway <philip.ridgeway@gmail.com>
2018-08-15 11:04:12 -0700

    Feature Layer Extrusion: Uses absoluteHeight extrusion mode. (#149)
    * Feature Layer Extrusion: Uses absoluteHeight extrusion mode.
    
    Also implements fix for extrusion over Alaska.
    
    * Changes precondition failure to an assertion failure.

--- arcgis-runtime-samples-macos/Scenes/Feature layer extrusion/FeatureLayerExtrusionViewController.swift
+++ arcgis-runtime-samples-macos/Scenes/Feature layer extrusion/FeatureLayerExtrusionViewController.swift
@@ -16,68 +16,78 @@
 import Cocoa
 import ArcGIS
 
+/// A view controller that manages the interface of the Feature Layer Extrusion
+/// sample.
 class FeatureLayerExtrusionViewController: NSViewController {
-
-    @IBOutlet weak var extrusionControl: NSSegmentedControl!
+    /// The scene displayed in the scene view.
+    let scene: AGSScene
+    /// The renderer of the feature layer.
+    let renderer: AGSRenderer
+    
+    required init?(coder: NSCoder) {
+        scene = AGSScene(basemapType: .topographic)
+        
+        // Create service feature table from US census feature service.
+        let table = AGSServiceFeatureTable(url: .censusMapServiceStatesLayer)
+        // Create feature layer from service feature table.
+        let layer = AGSFeatureLayer(featureTable: table)
+        // Feature layer must be rendered dynamically for extrusion to work.
+        layer.renderingMode = .dynamic
+        // Setup the symbols used to display the features (US states) from the table.
+        let lineSymbol = AGSSimpleLineSymbol(style: .solid, color: .blue, width: 1.0)
+        let fillSymbol = AGSSimpleFillSymbol(style: .solid, color: .blue, outline: lineSymbol)
+        renderer = AGSSimpleRenderer(symbol: fillSymbol)
+        if let sceneProperties = renderer.sceneProperties {
+            sceneProperties.extrusionMode = .absoluteHeight
+            sceneProperties.extrusionExpression = Statistic.totalPopulation.extrusionExpression
+        }
+        
+        // Set the renderer on the layer and add the layer to the scene.
+        layer.renderer = renderer
+        scene.operationalLayers.add(layer)
+        
+        super.init(coder: coder)
+    }
+    
+    /// The scene view managed by the view controller.
     @IBOutlet weak var sceneView: AGSSceneView!
-    // initialize scene with the topographic basemap
-    let scene = AGSScene(basemapType: .topographic)
-
-    // US census data feature service
-    let statesService = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3"
-    var renderer : AGSRenderer?
+    /// The segmented control used to select the statistic that is extruded.
+    @IBOutlet weak var extrusionControl: NSSegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.sceneView.scene = scene
+        sceneView.scene = scene
         
-        // create service feature table from US census feature service
-        let table = AGSServiceFeatureTable(url: URL(string: statesService)!)
-        // create feature layer from service feature table
-        let layer = AGSFeatureLayer(featureTable: table)
-        // feature layer must be rendered dynamically for extrusion to work
-        layer.renderingMode = .dynamic
-        // setup the symbols used to display the features (US states) from the table
-        let lineSymbol = AGSSimpleLineSymbol(style:.solid, color: .blue, width: 1.0)
-        let fillSymbol = AGSSimpleFillSymbol(style:.solid, color: .blue, outline: lineSymbol)
-        renderer = AGSSimpleRenderer(symbol: fillSymbol)
-        // need to set an extrusion type, BASE HEIGHT extrudes each point from the feature individually
-        renderer?.sceneProperties?.extrusionMode = .baseHeight
-        
-        // set the extrusion to total population
-        renderer?.sceneProperties?.extrusionExpression = "[POP2007]/ 10"
-        extrusionControl.selectedSegment = 0
-
-        // set the renderer on the layer and add the layer to the scene
-        layer.renderer = renderer
-        self.scene.operationalLayers.add(layer)
-        
-        // set the initial view
+        // Set the initial view.
         let initialLocation = AGSPoint(x: -98.585522, y: 70, spatialReference: sceneView.spatialReference)
-        let orbitCamera = AGSOrbitLocationCameraController(targetLocation: initialLocation, distance: 15000000)
+        let orbitCamera = AGSOrbitLocationCameraController(targetLocation: initialLocation, distance: 15_000_000)
         orbitCamera.cameraPitchOffset = 75.0
         sceneView.cameraController = orbitCamera
-
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-    @IBAction func extrusionAction(_ sender: NSSegmentedControl) {
-        // button action for extruding by total population or by population density
-        switch sender.selectedSegment {
-        case 0:
-            renderer?.sceneProperties?.extrusionExpression = "[POP2007]/ 10"
-        case 1:
-            renderer?.sceneProperties?.extrusionExpression = "[POP07_SQMI] * 5000"
-        default:
-            renderer?.sceneProperties?.extrusionExpression = ""
+    
+    enum Statistic: Int {
+        case totalPopulation
+        case populationDensity
+        
+        /// The extrusion expression for the statistic.
+        var extrusionExpression: String {
+            switch self {
+            case .totalPopulation:
+                return "[POP2007]/ 10"
+            case .populationDensity:
+                // The offset makes the extrusion look better over Alaska.
+                let offset = 100_000
+                return "([POP07_SQMI] * 5000) + \(offset)"
+            }
         }
     }
     
+    @IBAction func extrusionAction(_ sender: NSSegmentedControl) {
+        if let statistic = Statistic(rawValue: sender.selectedSegment) {
+            renderer.sceneProperties?.extrusionExpression = statistic.extrusionExpression
+        } else {
+            assertionFailure("Selected segment does not correspond to a statistic")
+        }
+    }
 }
-
