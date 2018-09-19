c2f174015dbc4b90f7a4837947f38c6ae3df6374
Philip Ridgeway <philip.ridgeway@gmail.com>
2018-09-19 09:55:53 -0700

    Conforms QueryMapImageSublayerViewController.SublayerKey to CaseIterable (#176)

--- arcgis-runtime-samples-macos/Layers/Query a map image sublayer/QueryMapImageSublayerViewController.swift
+++ arcgis-runtime-samples-macos/Layers/Query a map image sublayer/QueryMapImageSublayerViewController.swift
@@ -65,15 +65,10 @@ class QueryMapImageSublayerViewController: NSViewController {
         enableControlsIfNeeded()
     }
     
-    enum SublayerKey: Int {
+    enum SublayerKey: Int, CaseIterable {
         case cities = 0
         case states = 2
         case counties = 3
-        
-        /// A set of all values of this type.
-        static var allCases: Set<SublayerKey> {
-            return [.cities, .states, .counties]
-        }
     }
     
     /// The sublayers of the map image layer.
