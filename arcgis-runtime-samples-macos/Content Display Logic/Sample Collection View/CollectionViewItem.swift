13b603738b0f9db03dce126836b38c6c7ae0bb4a
Philip Ridgeway <pridgeway@esri.com>
2018-10-04 17:24:25 -0700

    Specifies that the cell's header view always has a dark appearance
    That makes the header's text always appear as light-on-dark.

--- arcgis-runtime-samples-macos/Content Display Logic/Sample Collection View/CollectionViewItem.swift
+++ arcgis-runtime-samples-macos/Content Display Logic/Sample Collection View/CollectionViewItem.swift
@@ -32,6 +32,14 @@ class CollectionViewItem: NSCollectionViewItem {
 }
 
 class CollectionViewItemHeaderView: NSView {
+    @IBOutlet var textField: NSTextField! {
+        didSet {
+            if #available(OSX 10.14, *) {
+                textField.appearance = NSAppearance(named: .darkAqua)
+            }
+        }
+    }
+    
     override func updateLayer() {
         layer?.backgroundColor = NSColor.appBlue.cgColor
     }
