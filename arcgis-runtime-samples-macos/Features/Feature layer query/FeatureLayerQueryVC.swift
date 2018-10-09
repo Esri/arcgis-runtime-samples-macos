535d574953afdb9dd2170a6dc29ff77cc856c9c4
Quincy Morgan <qmorgan@esri.com>
2018-10-09 15:12:46 -0700

    Updates the data source of and modernizes the Feature Layer Query sample

--- arcgis-runtime-samples-macos/Features/Feature layer query/FeatureLayerQueryVC.swift
+++ arcgis-runtime-samples-macos/Features/Feature layer query/FeatureLayerQueryVC.swift
@@ -17,99 +17,105 @@
 import Cocoa
 import ArcGIS
 
-class FeatureLayerQueryVC: NSViewController, NSTextFieldDelegate {
+class FeatureLayerQueryVC: NSViewController {
     
     @IBOutlet private weak var mapView:AGSMapView!
     
-    private var map:AGSMap!
-    private var featureTable:AGSServiceFeatureTable!
-    private var featureLayer:AGSFeatureLayer!
+    private var featureTable:AGSServiceFeatureTable?
+    private var featureLayer:AGSFeatureLayer?
     
     private var selectedFeatures = [AGSFeature]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //initialize map with topographic basemap
-        self.map = AGSMap(basemap: .topographic())
-        //assign map to the map view
-        self.mapView.map = self.map
+        // initialize map with topographic basemap
+        let map = AGSMap(basemap: .topographic())
+        // assign map to the map view
+        mapView.map = map
         
         /// The url of a map service layer containing sample census data of United States counties.
-        let censusMapServiceCountiesLayerURL = URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/2")!
-        //create feature table using a url
-        self.featureTable = AGSServiceFeatureTable(url: censusMapServiceCountiesLayerURL)
-        //create feature layer using this feature table
-        self.featureLayer = AGSFeatureLayer(featureTable: self.featureTable)
+        let statesFeatureTableURL = URL(string: "https://services.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/USA_Daytime_Population_2016/FeatureServer/0")!
+        // create feature table using a url
+        let featureTable = AGSServiceFeatureTable(url: statesFeatureTableURL)
+        self.featureTable = featureTable
         
-        //set a new renderer
+        // create feature layer using this feature table
+        let featureLayer = AGSFeatureLayer(featureTable: featureTable)
+        self.featureLayer = featureLayer
+        // show the layer at all scales
+        featureLayer.minScale = 0
+        featureLayer.maxScale = 0
+        
+        // set a new renderer
         let lineSymbol = AGSSimpleLineSymbol(style: .solid, color: .black, width: 1)
         let fillSymbol = AGSSimpleFillSymbol(style: .solid, color: NSColor.yellow.withAlphaComponent(0.5), outline: lineSymbol)
-        self.featureLayer.renderer = AGSSimpleRenderer(symbol: fillSymbol)
+        featureLayer.renderer = AGSSimpleRenderer(symbol: fillSymbol)
         
-        //add feature layer to the map
-        self.map.operationalLayers.add(self.featureLayer)
-        //zoom to a custom viewpoint
-        self.mapView.setViewpointCenter(AGSPoint(x: -11e6, y: 5e6, spatialReference: AGSSpatialReference.webMercator()), scale: 9e7, completion: nil)
+        // add feature layer to the map
+        map.operationalLayers.add(featureLayer)
+        
+        // center the layer
+        mapView.setViewpointCenter(AGSPoint(x: -11e6, y: 5e6, spatialReference: AGSSpatialReference.webMercator()), scale: 9e7)
     }
     
-    func queryForState(_ state:String) {
-        //un select if any features already selected
-        if self.selectedFeatures.count > 0 {
-            self.featureLayer.unselectFeatures(self.selectedFeatures)
-            self.selectedFeatures.removeAll()
+    private func selectFeaturesForSearchTerm(_ searchTerm:String) {
+        
+        guard let featureLayer = featureLayer,
+            let featureTable = featureTable else {
+            return
         }
         
-        //show progress indicator
+        // deselect all selected features
+        if selectedFeatures.count > 0 {
+            featureLayer.unselectFeatures(selectedFeatures)
+            selectedFeatures.removeAll()
+        }
+        
+        // show progress indicator
         NSApp.showProgressIndicator()
         
         let queryParams = AGSQueryParameters()
-        queryParams.whereClause = "upper(STATE_NAME) LIKE '%\(state.uppercased())%'"
+        queryParams.whereClause = "upper(STATE_NAME) LIKE '%\(searchTerm.uppercased())%'"
         
-        self.featureTable.queryFeatures(with: queryParams) { [weak self] (result:AGSFeatureQueryResult?, error:Error?) -> Void in
+        featureTable.queryFeatures(with: queryParams) { [weak self] (result:AGSFeatureQueryResult?, error:Error?) -> Void in
             
-            //hide progress indicator
+            // hide progress indicator
             NSApp.hideProgressIndicator()
             
+            guard let self = self else {
+                return
+            }
+            
             if let error = error {
-                //show error
-                self?.showAlert(messageText: "Error", informativeText: error.localizedDescription)
+                // display the error as an alert
+                NSAlert(error: error).beginSheetModal(for: self.view.window!)
             }
             else if let features = result?.featureEnumerator().allObjects {
                 if features.count > 0 {
-                    self?.featureLayer.select(features)
-                    //zoom to the selected feature
-                    self?.mapView.setViewpointGeometry(features[0].geometry!, padding: 200, completion: nil)
-                }
-                else {
-                    self?.showAlert(messageText: "Alert", informativeText: "No state by that name")
+                    // display the selection
+                    featureLayer.select(features)
+                    
+                    // zoom to the selected feature
+                    self.mapView.setViewpointGeometry(features[0].geometry!, padding: 200)
+                
+                } else {
+                    if let fullExtent = featureLayer.fullExtent {
+                        // no matches, zoom to show everything in the layer
+                        self.mapView.setViewpointGeometry(fullExtent, padding: 50)
+                    }
                 }
                 
-                //update selected features array
-                self?.selectedFeatures = features
+                // update selected features array
+                self.selectedFeatures = features
             }
         }
     }
     
-    //MARK: - NSTextFieldDelegate
-    
-    func controlTextDidEndEditing(_ obj: Notification) {
-        if let searchField = obj.object as? NSSearchField {
-            
-            //if field has some value then query
-            //else do nothing
-            if !searchField.stringValue.isEmpty {
-                self.queryForState(searchField.stringValue)
-            }
+    @IBAction func searchFieldAction(_ sender: NSSearchField) {
+        let query = sender.stringValue.trimmingCharacters(in: .whitespacesAndNewlines)
+        if !query.isEmpty {
+            selectFeaturesForSearchTerm(query)
         }
-    }
-    
-    //MARK: - Helper methods
-    
-    private func showAlert(messageText:String, informativeText:String) {
-        let alert = NSAlert()
-        alert.messageText = messageText
-        alert.informativeText = informativeText
-        alert.beginSheetModal(for: self.view.window!)
     }
 }
